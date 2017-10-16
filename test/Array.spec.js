96306066d830fbe6cc33213ea922ed666a9803b5
zzuieliyaoli <zzuieliyaoli@gmail.com>
2017-10-17 00:08:40 +0800

    Using deconstruction

--- test/Array.spec.js
+++ test/Array.spec.js
@@ -1,10 +1,12 @@
 const { expect } = require('chai');
 const { array } = require('../src');
 
+const { swap } = array;
+
 describe('Array', () => {
   it('should swap two elements of array', () => {
     const a = [1, 2];
-    array.swap(a, 0, 1);
+    swap(a, 0, 1);
     expect(a).to.eql([2, 1]);
   });
 });
--- test/Tree.spec.js
+++ test/Tree.spec.js
@@ -1,10 +1,12 @@
 const { expect } = require('chai');
 const { tree } = require('../src');
 
+const { arrayToTree, treeToArray } = tree;
+
 describe('Tree', () => {
   it('should convert array to tree', () => {
     const a = [1, 2, 3, null, 4];
-    const t = tree.arrayToTree(a);
-    expect(tree.treeToArray(t)).to.eql(a);
+    const t = arrayToTree(a);
+    expect(treeToArray(t)).to.eql(a);
   });
 });

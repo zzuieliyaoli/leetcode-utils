0a84a9c3809ad459409e3644ccd139bcbd0c14fc
zzuieliyaoli <zzuieliyaoli@gmail.com>
2017-10-18 23:22:58 +0800

    Replace iterator with recursion to fix listToArray

--- test/List.spec.js
+++ test/List.spec.js
@@ -18,9 +18,7 @@ describe('List', () => {
     expect(listToArray(null)).to.eql([]);
   });
 
-  // TODO FIX
-  // Error: FATAL ERROR: invalid array length Allocation failed - JavaScript heap out of memory
-  it.skip('should convert array to list', () => {
+  it('should convert array to list', () => {
     const a = [1, 2, 3, 4];
     const l = arrayToList(a);
     expect(listToArray(l)).to.deep.equal(a);
